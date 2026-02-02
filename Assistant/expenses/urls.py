67cea44b93bd622d20234555b1969fb3f85c6b21
Jasper <Jasper.twn@gmail.com>
2026-02-02 05:24:49 +0000

    Connections Finished

--- Assistant/expenses/urls.py
+++ Assistant/expenses/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+    path('', ExpenseList.as_view(), name='expense_list'), 
+    path('create/', ExpenseCreate.as_view(), name='expense_create'), 
+    path('<int:pk>/update/', ExpenseUpdate.as_view(), name='expense_update'), 
+    path('<int:pk>/delete/', ExpenseDelete.as_view(), name='expense_delete'),
 ]
